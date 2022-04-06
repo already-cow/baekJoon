3031ab03c8dfafc848e05522fb896aff180bce3d
alreadycow <alreadycow_@naver.com>
2022-04-06 19:42:37 +0900

    first commit

--- python/2525_오븐시계.py
+++ python/2525_오븐시계.py
@@ -1,13 +1,12 @@
 hour, min = map(int,input().split())
 set = int(input())
 
-if hour < 23 and min+set >= 60:
-    a = (min+set)//60
-    b = (min+set)%60
-    print(hour+a, b)
-elif hour == 23 and min+set >= 60:
-    a = (min+set)//60
-    b = (min+set)%60
-    print(a-1,b)
+if min+set >= 60:
+    a = (min+set) // 60
+    b = (min+set) % 60
+    if hour+a >= 24:
+        print(hour+a-24,b)
+    elif hour+a < 24:
+        print(hour+a,b)
 else:
     print(hour, min+set)
