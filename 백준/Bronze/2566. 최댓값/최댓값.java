4a84f4064e917565d590cffce850de711b1f610f
already-cow <97944505+already-cow@users.noreply.github.com>
2024-04-01 12:09:29 +0900

    [Bronze III] Title: 최댓값, Time: 144 ms, Memory: 16012 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,9 +1,10 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
         int[][] arr = new int[9][9];
 
         int max = 0;
@@ -11,9 +12,9 @@ public class Main {
         int b = 0;
 
         for (int i = 0; i < 9; i++) {
-            String[] str = br.readLine().split(" ");
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < 9; j++) {
-                arr[i][j] = Integer.parseInt(str[j]);
+                arr[i][j] = Integer.parseInt(st.nextToken());
 
                 if (arr[i][j] >= max) {
                     max = arr[i][j];
