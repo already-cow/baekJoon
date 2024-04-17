f09e3b1d8fdcac6b8bb3e464291720215a2b1f8e
already-cow <97944505+already-cow@users.noreply.github.com>
2024-04-17 10:17:04 +0900

    [Bronze V] Title: A+B - 7, Time: 160 ms, Memory: 16196 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            int sum = a + b;
+
+            System.out.println("Case #" + i + ": " + sum);
+            }
+
+
+
+        }
+    }
